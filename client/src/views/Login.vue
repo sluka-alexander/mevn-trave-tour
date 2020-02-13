44094abf1054cadb120964c1654078858e01f183
Sluka Alexander <slukasane@gmail.com>
2020-02-14 01:57:42 +0300

    'first'

---
+++ client/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <div class="login__title">
+        <h1>Login</h1>
+    </div>
+      <input type="text" placeholder="Enter login">
+      <input type="password" placeholder="Enter password">
+    <button>Sign in</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+};
+</script>
+
+<style scoped>
+  .login{
+
+    margin: 0 auto;
+    max-width: 400px;
+    background-color: white;
+  }
+</style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>Register</h1>
+    <div class="register-form">
+      <input type="text" name="password" placeholder="Enter name">
+      <input type="email" name="password" placeholder="Enter email">
+      <input type="password" name="password" placeholder="Enter password">
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .register-form {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+
+    input {
+      font-size: 18px;
+      padding: 20px;
+      margin-bottom: 10px;
+      outline: 0;
+    }
+  }
+</style>
